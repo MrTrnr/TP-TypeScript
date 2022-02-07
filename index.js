3eefc9ed56dd182a2044b2551e5eedeb0af2428b
MrTrnr <marietournier.pro@gmail.com>
2022-02-07 15:24:03 +0100

    raz tp

--- index.js
+++
@@ -1,6 +0,0 @@
-import {Presentation } from './presentation.js';
-import {Service} from './service.js';
-
-const service = new Service();
-const presentation = new Presentation(service);
-presentation.demarrer(); 
